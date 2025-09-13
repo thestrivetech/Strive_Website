c625fe88ab8276b2b0df22f04ed85e2fc8575dda
JGRamey <jgrameyZO@Outlook.com>
2025-09-13 13:26:46 -0400

    Update resources.tsx

--- client/src/pages/resources.tsx
+++ client/src/pages/resources.tsx
@@ -776,8 +776,7 @@ const Resources = () => {
                       <Eye className="ml-1 h-3 w-3 md:ml-2 md:h-4 md:w-4" />
                     </Button>
                   </CardContent>
-                </div>
-              </Card>
+                </Card>
             ))}
             </div>
           )}
