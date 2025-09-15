0a8fddcce55689d013ab58b05c3fe8c29f762319
JGRamey <jgrameyZO@Outlook.com>
2025-09-14 22:54:48 -0400

    Form Fix?

--- dist/index.js
+++ dist/index.js
@@ -1861,8 +1861,8 @@ app.use((req, res, next) => {
   }
   if (!process.env.VERCEL) {
     const port = parseInt(process.env.PORT || "5000", 10);
-    const host = "0.0.0.0";
-    const options = { port, host };
+    const host = process.platform === "win32" ? "127.0.0.1" : "0.0.0.0";
+    const options = process.platform === "win32" ? { port, host } : { port, host, reusePort: true };
     server.listen(options, () => {
       log2(`serving on port ${port} (${host})`);
     });
